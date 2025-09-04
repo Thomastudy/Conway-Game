8253cfb4ee49c239f215723d8a382ddc07f84597
Thomas <thomandoclases@gmail.com>
2025-09-04 12:35:07 -0300

    tablero creo que creado

--- src/Program/Tablero.cs
+++ src/Program/Tablero.cs
@@ -2,51 +2,14 @@ namespace Ucu.Poo.GameOfLife;
 
 public class Tablero
 {
-    bool[,] gameBoard = /* contenido del tablero */;
-    int boardWidth = gameBoard.GetLength(0);
-    int boardHeight = gameBoard.GetLength(1);
-
-    bool[,] cloneboard = new bool[boardWidth, boardHeight];
-        for (int x = 0; x < boardWidth; x++)
+    public bool[,] Board { get; set; } 
+    public int Height { get; set; }
+    public int Width { get; set; }
+    
+   public Tablero(bool[,] initialBoard)
     {
-        for (int y = 0; y < boardHeight; y++)
-        {
-            int aliveNeighbors = 0;
-            for (int i = x-1; i<=x+1;i++)
-            {
-                for (int j = y-1;j<=y+1;j++)
-                {
-                    if(i>=0 && i<boardWidth && j>=0 && j < boardHeight && gameBoard[i,j])
-                    {
-                        aliveNeighbors++;
-                    }
-                }
-            }
-            if(gameBoard[x,y])
-            {
-                aliveNeighbors--;
-            }
-            if (gameBoard[x,y] && aliveNeighbors < 2)
-            {
-                //Celula muere por baja población
-                cloneboard[x,y] = false;
-            }
-            else if (gameBoard[x,y] && aliveNeighbors > 3)
-            {
-                //Celula muere por sobrepoblación
-                cloneboard[x,y] = false;
-            }
-            else if (!gameBoard[x,y] && aliveNeighbors == 3)
-            {
-                //Celula nace por reproducción
-                cloneboard[x,y] = true;
-            }
-            else
-            {
-                //Celula mantiene el estado que tenía
-                cloneboard[x,y] = gameBoard[x,y];
-            }
-        }
+        this.Board = initialBoard;
+        this.Width = initialBoard.GetLength(0);
+        this.Height = initialBoard.GetLength(1);
     }
-    gameBoard = cloneboard;
 }
