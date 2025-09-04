90e49ff67c5864e57442d7dc1df3ab167f095806
Ezequiel Pasto <ezequiel.pastorino@correo.ucu.edu.uy>
2025-09-04 12:40:16 -0300

    todo hasta ahora

---
+++ src/Program/Lector.cs
@@ -0,0 +1,23 @@
+using System.IO;
+namespace Ucu.Poo.GameOfLife;
+
+public class Lector
+{
+    public Tablero Leer(string url)
+    {
+        string content = File.ReadAllText(url);
+        string[] contentLines = content.Split('\n');
+        Tablero board = new Tablero[contentLines.Length, contentLines[0].Length];
+        for (int y = 0; y < contentLines.Length; y++)
+        {
+            for (int x = 0; x < contentLines[y].Length; x++)
+            {
+                if (contentLines[y][x] == '1')
+                {
+                    board[x, y] = true;
+                }
+            }
+            return board;
+        }
+    }
+}
