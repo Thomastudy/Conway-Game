69acaa88bc2a5d8b1621fb373d87fa5dbde4a18e
Facundo Gallinal <facundo.gallinal@correo.ucu.edu.uy>
2025-09-04 12:36:18 -0300

    todo lo hecho hasta ahora

--- src/Program/Impresor.cs
+++ src/Program/Impresor.cs
@@ -1,10 +1,37 @@
+using System;
+using System.Text;
+using System.Threading;
+
 namespace Ucu.Poo.GameOfLife;
 
 public class Impresor
 {
-    public void Print(Tablero tablero) 
+    public void Print(Tablero tablero)
     {
-        
-        
-    } 
+        int width; //variabe que representa el ancho del tablero
+        int height; //variabe que representa altura del tablero
+        while (true)
+        {
+            Console.Clear();
+            StringBuilder s = new StringBuilder();
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < width; x++)
+                {
+                    if (tablero[x, y])
+                    {
+                        s.Append("|X|");
+                    }
+                    else
+                    {
+                        s.Append("___");
+                    }
+                }
+                s.Append("\n");
+            }
+            Console.WriteLine(s.ToString());
+            NextGen.Calculo(tablero);
+            Thread.Sleep(300);
+        }
+    }
 }
