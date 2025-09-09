36fef7e3ae7bef3d4d3584b176110c0f02259e9a
Facundo Gallinal <facundo.gallinal@correo.ucu.edu.uy>
2025-09-09 12:52:14 -0300

    cambio program

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,1 +1,15 @@
-﻿
+﻿using System;
+
+namespace Ucu.Poo.GameOfLife
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Lector lector = new Lector();
+            Tablero tablero = lector.Leer("board.txt");
+            Impresor impresor = new Impresor();
+            impresor.Print(tablero);
+        }
+    }
+}
